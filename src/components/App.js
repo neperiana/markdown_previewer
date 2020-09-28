a6542eeff7561673a073acc76b3ae7180e43310a
neperiana <ikbenmila@gmail.com>
2020-09-28 20:54:47 +0100

    Finished layout/design and started react coding

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,36 @@
+// React
 import React, { Component } from 'react';
+
+// CSS
 import './App.css';
 
+// Resources
+import example_md from '../resources';
+
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      input: '',
+      html: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  handleChange(event) {
+    let input = event.target.value;
+    let html = input;
+    this.setState({
+      input: input,
+      html: html
+    });
+  }
+  componentDidMount() {
+    console.log(example_md)
+    this.state = {
+      input: example_md
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -9,22 +38,26 @@ class App extends Component {
           <h2>Markdown Previewer</h2>
         </div>
 
-        <div className="markdown-area">
-          <h3>Markdown</h3>
-          <textarea name="markdown-textarea">
-            The cat was playing in the garden.
-          </textarea>
-        </div>
-        
-        <div className="html-area">
-          <div><h3>HTML</h3></div>
-          <div className="html-rendered">
-            <p><em>Hola! </em>The cat was playing in the garden.</p>
+        <div className="containers">
+
+          <div className="markdown-area">
+            <h3>Markdown</h3>
+            <textarea name="markdown-textarea" onChange={this.handleChange}>
+              {this.state.input}
+            </textarea>
           </div>
-        </div>
+          
+          <div className="html-area">
+            <div><h3>HTML</h3></div>
+            <div className="html-rendered">
+              <p><em> {this.state.html} </em></p>
+            </div>
+          </div>
+
+        </div> 
 
         <div className="Footer">
-          <p>Copyright 2020, camila. Thanks to <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> and <a href="https://type.fit/" title="Type.fit">Type.fit</a>.</p>
+          <p>	&#169; 2020, camila. Thanks to <a href="https://cdnjs.com/libraries/marked" title="marked">marked</a>.</p>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+// React modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// React components
 import App from './components/App';
+
+// CSS
 import './index.css';
 
 ReactDOM.render(
